7051588918c59b33704cbc1b6ad43f439cc72350
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2018-12-21 01:16:47 -0200

    Update authProviderStrapi.js

--- authProviderStrapi.js
+++ authProviderStrapi.js
@@ -5,27 +5,27 @@ export default (type, params) => {
     // called when the user attempts to log in
     if (type === AUTH_LOGIN) {
         const { username } = params;
-        localStorage.setItem('username', username);
+        localStorage.setItem('token', username);
         // accept all username/password combinations
         return Promise.resolve();
     }
     // called when the user clicks on the logout button
     if (type === AUTH_LOGOUT) {
-        localStorage.removeItem('username');
+        localStorage.removeItem('token');
         return Promise.resolve();
     }
     // called when the API returns an error
     if (type === AUTH_ERROR) {
         const { status } = params;
         if (status === 401 || status === 403) {
-            localStorage.removeItem('username');
+            localStorage.removeItem('token');
             return Promise.reject();
         }
         return Promise.resolve();
     }
     // called when the user navigates to a new location
     if (type === AUTH_CHECK) {
-        return localStorage.getItem('username')
+        return localStorage.getItem('token')
             ? Promise.resolve()
             : Promise.reject();
     }
